f3503a0dbbe67de43ae437f314a7c1dc52624e80
glancy <valentindesel@gmail.com>
2025-07-06 09:48:46 +0200

    added setting.py

--- website/config/settings.py
+++ website/config/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
-    'blog',
-    'projects',
+    'website.core',
+    'website.blog',
+    'website.projects',
 
 ]
 
