ecf384d518fa22d99b978bf3503c0f0b948654fa
glancy <valentindesel@gmail.com>
2025-07-06 15:52:13 +0200

    temp admin

--- core/views.py
+++ core/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "core/home.html")
+
+def create_admin(request):
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("glancy", "valentindesel@gmail.com", "Valick565+")
+        return HttpResponse("Admin created")
+    return HttpResponse("Admin already exists")
