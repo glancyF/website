1a9fffe17de2d3f381635a2887e00cc41e46a45d
glancy <valentindesel@gmail.com>
2025-07-06 15:57:47 +0200

    temp admins.py

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from core.views import create_admin
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("core.urls")),
